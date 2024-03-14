19634ed6fb37a46416ac8655a4acf79194d6a7c9
mfaishal82 <mfaishal82@yahoo.co.id>
2024-03-14 11:12:17 +0700

    feat: route pub with 3rd party api

--- server/routes/pub.js
+++ server/routes/pub.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const { getData, getDataById } = require('../controllers/pubController')
 const router = express.Router()
 
-router.get('/')
+router.get('/', getData)
+router.get('/:id', getDataById)
 
 module.exports = router
